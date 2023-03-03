a23159f058fdcb88d969ebaf4283bd4d80c88806
al-ramsey <s2158261@ed.ac.uk>
2023-03-03 15:08:25 +0000

    started 06

--- src/exercises _live_23.01.2023/06_sub_sequences.lean
+++ src/exercises _live_23.01.2023/06_sub_sequences.lean
@@ -52,7 +52,13 @@ inputs. -/
 -- 0039
 lemma extraction_ge : extraction φ → ∀ N N', ∃ n ≥ N', φ n ≥ N :=
 begin
-  sorry
+  intros hyp N N',
+  unfold extraction at hyp,
+  use max N N',
+  split,
+  { exact le_max_right N N', },
+  calc N ≤ max N N' : le_max_left N N'
+  ... ≤ φ (max N N') : by apply id_le_extraction hyp,
 end
 
 /-- A real number `a` is a cluster point of a sequence `u` 
